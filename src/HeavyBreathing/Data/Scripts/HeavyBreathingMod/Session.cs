993a763497c13646546f656b170b314d916844ca
Will Keith <wakjah@gmail.com>
2021-07-24 09:56:24 +0100

    Increase multiplier to 16 so each full refill is closer to 1% of an oxygen tank. Increase the capacity of oxygen bottles by the same multiplier so they are useful again

--- src/HeavyBreathing/Data/Scripts/HeavyBreathingMod/Session.cs
+++ src/HeavyBreathing/Data/Scripts/HeavyBreathingMod/Session.cs
@@ -6,9 +6,10 @@ namespace EvilElectricCorpMod
     [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
     public class Session : MySessionComponentBase
     {
-        private static readonly float Multiplier = 12;
+        private static readonly float Multiplier = 16;
         private static readonly float StorageCapacity = 60 * Multiplier;
         private static readonly float Consumption = 0.063f;
+        private static readonly float OxygenBottleCapacity = 100 * Multiplier;
 
         public override void BeforeStart()
         {
@@ -24,6 +25,14 @@ namespace EvilElectricCorpMod
                     {
                         storage.MaxCapacity = StorageCapacity;
                     }
+                }
+            }
+
+            foreach (var definition in definitions.GetDefinitionsOfType<MyOxygenContainerDefinition>())
+            {
+                if (definition.StoredGasId.SubtypeName == "Oxygen")
+                {
+                    definition.Capacity = OxygenBottleCapacity;
                 }
             }
         }
